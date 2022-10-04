9359d736c6415756100aef670a82f2f59e1a5d5b
Roger Waldron <roger.waldron@gmail.com>
2022-10-04 23:35:06 +0300

    feat: :sparkles: Cnofigured NextJS with Styled Components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import GlobalStyle from "@/components/globalstyles";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#111",
+    secondary: "#0070f3",
+  },
+};
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default App;
