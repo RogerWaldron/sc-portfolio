a77f5cf23b3a7f24ee85de6779da3025cdf7832d
Roger Waldron <roger.waldron@gmail.com>
2022-10-05 14:56:52 +0300

    style: :lipstick: Fix Poppins now loading correctly

--- components/fontstyles.tsx
+++ components/fontstyles.tsx
@@ -3,9 +3,9 @@ import { createGlobalStyle } from "styled-components";
 const FontStyles = createGlobalStyle`
   @font-face {
     font-family: 'Poppins';
-    src: url("public/fonts/Poppins-Regular.ttf") format('truetype'),
-      url("public/fonts/Poppins-Bold.ttf") format('truetype'),
-      url("public/fonts/Poppins-Black.ttf") format('truetype');
+    src: url("/fonts/Poppins-Regular.ttf") format('truetype'),
+      url("/fonts/Poppins-Bold.ttf") format('truetype'),
+      url("/fonts/Poppins-Black.ttf") format('truetype');
 `;
 
 export default FontStyles;
